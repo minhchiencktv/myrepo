583914c9a915a18f919629e92024524df9d5a04a
minhchientnck <44605846+minhchientnck@users.noreply.github.com>
2020-10-28 14:36:29 +0700

    Add files via upload

---
+++ set_global_var_mysql.sql
@@ -0,0 +1,3 @@
+SET GLOBAL general_log = 'ON';
+SET GLOBAL general_log_file = 'D:\\phpMyAdmin_mysql_windows\\mysql\\data\\mysql_query.log'; 
+SET GLOBAL sql_mode = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
